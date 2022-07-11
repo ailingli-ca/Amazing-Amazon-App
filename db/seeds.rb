f6f7869b9b0aee9de282dbc5483be5f4a31c6b1c
Ailing Li <alofe9859@gmail.com>
2022-07-11 06:08:58 +0000

    Generate a Product model with 3 attributes; Create/Update/Delete  product in Rails console; Run seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+40.times do
+    Product.create(
+        title: Faker::Commerce.product_name,
+        description: Faker::Commerce.color,
+        price: Faker::Commerce.price
+    )
+end
+
+products = Product.all
+
+p "created #{products.count} products"

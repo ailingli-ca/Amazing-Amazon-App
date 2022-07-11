3039d63e37d4ba850bc3510f7670b1caadaa0b69
Ailing Li <alofe9859@gmail.com>
2022-07-11 08:05:09 +0000

    Product Model Callbacks - default price, title capitalize

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,19 @@ class Product < ApplicationRecord
 
     validates :price, numericality: {greater_than: 0}
     validates :description, presence: true, length: {mininum: 10}
+
+    # before_validation :set_default_price, :capitalize_title
+    before_save :capitalize_title
+
+    private
+
+    def set_default_price
+        self.price || = 1
+    end
+
+    def capitalize_title
+        self.title.capitalize!
+    end
+
+
 end

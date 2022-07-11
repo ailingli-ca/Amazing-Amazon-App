69b78ea2aefa71d7a8a718fa058c6d6debf4c0b1
Ailing Li <alofe9859@gmail.com>
2022-07-11 07:57:57 +0000

    Product Model Validations - title, price, description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+    validates :title, presence: true,
+    uniqueness: {case_sensitive: false} 
+
+    validates :price, numericality: {greater_than: 0}
+    validates :description, presence: true, length: {mininum: 10}
 end

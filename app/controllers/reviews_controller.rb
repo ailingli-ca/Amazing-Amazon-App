5ee30df850c1922a76228882bb4c4e1a82b1023b
Ailing Li <alofe9859@gmail.com>
2022-07-13 10:05:05 +0000

    4.2 Create Reviews Controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+    before_action :find_product
+
+    def create
+        @review = Review.new(params.require(:review).permit(:rating, :body))
+        @review.product = @product
+        if @review.save
+          redirect_to product_path(@product), notice: "review created!"
+        else
+          flash[:error] = "Invalid Review"
+          @reviews = @product.reviews.order(created_at: :desc)
+          render '/products/show', status: 303
+        #   redirect_to product_path(@product) #or 
+        end
+    end
+
+    private 
+
+    def find_product
+        @product= Product.find params[:product_id]
+    end
+end

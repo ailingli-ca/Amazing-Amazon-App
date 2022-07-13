20070ba064e3da0e01fb3313ae628341bb46181c
Ailing Li <alofe9859@gmail.com>
2022-07-13 10:17:57 +0000

    4.3 Reviews Controller - Destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,12 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def destroy
+        @review = Review.find params[:id]
+        @review.destroy
+        redirect_to product_path(@product), notice: "Review deleted"
+    end
+
     private 
 
     def find_product
